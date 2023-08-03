ba31aeaaf997572c6a929e81fef30ef265aea581
Alexis Thierry <alexist103@gmail.com>
2023-08-03 19:23:49 +0200

    [FIX] changing condition for shadow apparition on navBar

--- themes/BDE/src/js/main.js
+++ themes/BDE/src/js/main.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector('.navbar');
 
 function navbarShadow() {
-    if (window.scrollY >= 10) {
+    if (window.scrollY > 10) {
         navbar.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.25)';
     } else {
         navbar.style.boxShadow = 'none';
