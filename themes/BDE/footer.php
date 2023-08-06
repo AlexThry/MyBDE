2514835d01ccbe9a69d448f27032cbaf4f338660
Alexis Thierry <alexist103@gmail.com>
2023-08-06 16:13:21 +0200

    [fix] Changing footer location

--- themes/BDE/footer.php
+++ themes/BDE/footer.php
@@ -1,4 +1,13 @@
+    <footer class="footer">
+        <h1 class="section-title footer-title">
+            MyBDE
+        </h1>
+        <hr>
+        <div class="footer-content">
 
+        </div>
+    </footer>
 </body>
 <?php wp_footer() ?>
+
 </html>
--- themes/BDE/front-page.php
+++ themes/BDE/front-page.php
@@ -286,13 +286,4 @@
 
 </section>
 
-<footer class="footer">
-    <h1 class="section-title footer-title">
-        MyBDE
-    </h1>
-    <hr>
-    <div class="footer-content">
-
-    </div>
-</footer>
 <?php get_footer(); ?>
