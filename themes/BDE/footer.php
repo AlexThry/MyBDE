f0c9dca502cd0637b1d2e80a6201c334dc4dfdb6
Alexis Thierry <alexist103@gmail.com>
2023-08-08 23:47:12 +0200

    [Feature] Adding WPForms plugins

--- themes/BDE/footer.php
+++ themes/BDE/footer.php
@@ -1,16 +1,15 @@
-   <?php if (!is_page("registration")) : ?>
-   
-   <footer class="footer" style="">
-        <h1 class="section-title footer-title">
-            MyBDE
-        </h1>
-        <hr>
-        <div class="footer-content">
-
-        </div>
-    </footer>
-    <?php endif ?>
 </body>
+<?php if (!is_page("registration") && !is_page('my-account')) : ?>
+    <footer class="footer" style="">
+         <h1 class="section-title footer-title">
+             MyBDE
+         </h1>
+         <hr>
+         <div class="footer-content">
+    
+         </div>
+     </footer>
+ <?php endif ?>
 <?php wp_footer() ?>
 
 </html>
--- themes/BDE/header.php
+++ themes/BDE/header.php
@@ -24,11 +24,23 @@
                 <?php endif ?>
                 <?php if (is_user_logged_in()) : ?>
                     <!-- <a href="" class="button sign-up-button">Mon profile</a> -->
-                    <span class="user-name">Salut, <?php echo wp_get_current_user()->user_login ?></span>
-                    <a href="<?php echo wp_logout_url(home_url()) ?>" class="connection-button"><span>Logout</span></a>
+                    <a href="<?php echo site_url('my-account') ?>" class="button">Salut, <?php echo wp_get_current_user()->user_login ?> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                    <a href="<?php echo wp_logout_url(home_url()) ?>" class="connection-button"><span>Logout <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd" />
+                                <path fill-rule="evenodd" d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z" clip-rule="evenodd" />
+                            </svg>
+                        </span></a>
+
                 <?php else : ?>
                     <a href="<?php echo site_url("/registration") ?>" class="button sign-up-button">Sign-up</a>
-                    <a href="<?php echo wp_login_url() ?>" class="connection-button"><span>Login</span></a>
+                    <a href="<?php echo wp_login_url() ?>" class="connection-button"><span>Login <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd" />
+                                <path fill-rule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clip-rule="evenodd" />
+                            </svg>
+                        </span></a>
                 <?php endif ?>
 
             </div>
