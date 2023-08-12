8eae2e9aba072d1cd6107be8514d8e0805c8b2ba
Alexis Thierry <alexist103@gmail.com>
2023-08-12 18:56:09 +0200

    [FEATURE] Adding animation for logo

--- themes/BDE/header.php
+++ themes/BDE/header.php
@@ -11,9 +11,15 @@
 <body id="body" <?php body_class() ?>>
     <nav class="navbar">
         <a href="<?= is_front_page() ? "#body" : site_url()  ?>">
-            <h1 class="nav-title">
-                MyBDE
-            </h1>
+            <div class="nav-title">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="logo-home">
+                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                </svg>
+                <h1>
+                    MyBDE
+                </h1>
+            </div>
         </a>
         <?php if (!is_page("registration")) : ?>
             <div class="nav-buttons">
@@ -24,10 +30,11 @@
                 <?php endif ?>
                 <?php if (is_user_logged_in()) : ?>
                     <!-- <a href="" class="button sign-up-button">Mon profile</a> -->
-                    <a href="<?php echo site_url('my-account') ?>" class="button">Salut, <?php echo wp_get_current_user()->user_login ?> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <a href="<?php echo site_url('my-account') ?>" class="button profile-button">Salut, <?php echo wp_get_current_user()->first_name ?> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                             <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z" clip-rule="evenodd" />
                         </svg>
                     </a>
+                    <a href="<?php echo site_url('my-account') ?>" class="button small-profile-button">Profil</a>
                     <a href="<?php echo wp_logout_url(home_url()) ?>" class="connection-button"><span>Logout <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                                 <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd" />
                                 <path fill-rule="evenodd" d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z" clip-rule="evenodd" />
