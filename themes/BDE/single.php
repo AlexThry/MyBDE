4fe50e6c33031a1ab2cf43812e74d8c12962df28
Alexis Thierry <alexist103@gmail.com>
2023-08-29 08:13:13 +0200

    [FEATURE] Addding inscription form for event

--- themes/BDE/single.php
+++ themes/BDE/single.php
@@ -1,16 +1,20 @@
 <?php get_header() ?>
 
-<section class="post">
-    <?php
-    $tags = get_the_category();
-    display_tags($tags);
-    ?>
-    <h1 class="title"><?php the_title() ?></h1>
-    <hr>
-    <div class="content">
-        <?php the_content() ?>
-    </div>
+<?php
 
-</section>
+?>
 
-<?php get_footer() ?>
+    <section class="post">
+        <?php
+        $tags = get_the_category();
+        display_tags($tags);
+        ?>
+        <h1 class="title"><?php the_title() ?></h1>
+        <hr>
+        <div class="content">
+            <?php the_content() ?>
+        </div>
+        <?php bde_event_inscription_form(); ?>
+    </section>
+
+    <?php get_footer() ?>
